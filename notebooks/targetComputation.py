26cab9e44bd240d9b0178bc21964cc378fe056db
alexkern1997 <alexkern1997@gmail.com>
2021-04-02 08:22:07 +0200

    Cleaned 3 more notebooks
    Cleaned up 5 out of 6 notebooks

--- notebooks/targetComputation.py
+++ notebooks/targetComputation.py
@@ -29,7 +29,7 @@ def compute_PP_EDA(physio_data, pp):
 
     return processed
 
-def compute_EDA_Target(physio_data: pd.DataFrame) -> dict:
+def compute_EDA_Targets(physio_data: pd.DataFrame) -> dict:
     """Computes the EDA Target variables from an abritray dataframe containing the raw EDA signal. Returns these target variables in a dict."""
     
     pp = physio_data.pp.values[0] # Get the pp id
@@ -50,12 +50,12 @@ def compute_EDA_Target(physio_data: pd.DataFrame) -> dict:
     freq=round(1/(df.t_from_start.values[1] - df.t_from_start.values[0])) # Calculate the frequency at which the raw EDA signal was sampled 
     seconds = df.t_from_start.values[-1] - df.t_from_start.values[0] # Calculate the amount seconds the physio signal contains
     
-    signals, info = nk.eda_process(data.raw_EDA.dropna(), sampling_rate=freq) # Process the EDA signals using the eda_process function from NeuroKit2
+    signals, info = nk.eda_process(physio_data.raw_EDA.dropna(), sampling_rate=freq) # Process the EDA signals using the eda_process function from NeuroKit2
     
     ## Compute various EDA target variables and store these in the dict. Also add the pp id to the dict and return this dict
     processed['mean_SCL'] = signals.EDA_Tonic.mean() # Compute the mean SCL signal
     processed['corrected_mean_SCL'] = processed['mean_SCL'] - mean_Baseline # Compute the corrected mean SCL, by subtracting the mean SCL from the baseline component
-    processed['range_corrected_mean_SCL'] = ((signals.EDA_Tonic - min_Baseline) / (max_SCL - min_Baseline)).mean() # Compute the range corected min SCL, by Min Max scaling the signal and before computing the mean
+    processed['range_corrected_mean_SCL'] = ((signals.EDA_Tonic - min_Baseline) / (max_all - min_Baseline)).mean() # Compute the range corected min SCL, by Min Max scaling the signal and before computing the mean
     processed['standardised_mean_scl'] = ((signals.EDA_Tonic - mean_all) / std_all).mean() # Compute the standarised mean SCL, by standardising using the mean and std SCL throughout all the active components
     processed['frequency_NS_SCR'] = len(info['SCR_Peaks'])/seconds * 60 # Compute the frequency in which Non-Stimulus Skin Conductance Responses (NS-SCRs) occured, which is a EDA measures related to the Phasic component
     return processed
@@ -75,7 +75,7 @@ def compute_ECG_Targets(physio_data: pd.DataFrame) -> dict:
     freq=round(1/(physio_data.t_from_start.values[1] - physio_data.t_from_start.values[0])) # Calculate the frequency at which the raw EDA signal was sampled 
     seconds = physio_data.t_from_start.values[-1] - physio_data.t_from_start.values[0] # Calculate the amount seconds the physio signal contains
     
-    ecg_signals, info = nk.ecg_process(data["raw_ECG"], sampling_rate=freq) # Get the clean ECG signal using the ecg_process function from neurokit2    
+    ecg_signals, info = nk.ecg_process(physio_data["raw_ECG"], sampling_rate=freq) # Get the clean ECG signal using the ecg_process function from neurokit2    
     ecg_features = nk.ecg_intervalrelated(ecg_signals) # Compute HRV measures using the ecg_intervalrelated function from NeuroKit2
     
     ## Compute various HRV target variables and store these in the dict. Also add the pp id to the dict and return this dict
